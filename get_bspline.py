b3b1aa3cca48a478308e5508b662daf945476b2a
JoannaS11 <joanna.strobel@fau.de>
2024-06-19 15:53:44 +0200

    worked on finding local min, save copy

--- get_bspline.py
+++ get_bspline.py
@@ -121,7 +121,7 @@ def get_closest_point_on_spline(pcd, bSpline, normals_to_inside):
                 if np.dot(vector / np.linalg.norm(vector), normals[i] / np.linalg.norm(normals[i])) > - 1: # 45 ° in both directions
                     t_on_line[i] = t_old
                     vector_to_line[i] = vector
-                    if i % 20 == 0:
+                    if i % 200 == 0:
                         print(f"{np.round(i / np.shape(points)[0], decimals=3) * 100} % done")
                     break
             else:
@@ -176,115 +176,135 @@ def find_min_distances(vector_to_line_distances, t_on_line, bSpline):
     # sort by t
     ind = np.argsort(t_vec_combined[:,0] )
     t_vec_combined = t_vec_combined[ind]
+    create_bins_find_local_mins(t_vec_combined)
+    #find_points_to_pull(t_vec_combined,vector_to_line_distances, t_on_line, bSpline)
+
     plot_vectors(t_vec_combined[:,0], t_vec_combined[:,1], 0)
 
-def find_points_to_pull(vector_to_line_distances, t_on_line, bSpline):
+def create_bins_find_local_mins(t_vec_combined):
+    bin_size = 0.01
+    min_bin_arg = np.zeros([int(1/bin_size)])
+    for b_s in range(int(1/bin_size)):
+        bin_arg = np.argwhere(((t_vec_combined[:,0] >= bin_size * b_s) & (t_vec_combined[:,0] <= (b_s * bin_size+ bin_size))))
+        min_bin = np.argmin(t_vec_combined[bin_arg[:],1])
+        min_t = (t_vec_combined[bin_arg[:], 0]) [min_bin]
+        min_bin_arg[b_s] = np.argwhere(t_vec_combined[:, 0] == min_t)
+        print(f"bin_arg {bin_arg}")
 
-    values, counts = np.unique(t_on_line, return_counts=True)
-    print(values)
-    arg_counts = np.argwhere(counts != 1)
-    print(f"{np.shape(arg_counts)} {arg_counts}")
-    values_for_spline = np.full([np.shape(vector_to_line_distances)[0], 2], [-999.0, -999.0])
-    """print(f"va shape davor{np.shape(values_for_spline)}")
-    for i in range(np.shape(arg_counts)[0]):
-        print(i)
-        print(np.shape(values))
-        arg_where = np.argwhere(t_on_line == values[arg_counts[0][i]])
-        min = np.min(vector_to_line_distances[arg_where])
-        print(np.shape(np.array([values[arg_counts][i], min])))
-        values_for_spline[i] = np.array([values[arg_counts][i], min])
+    print(f"min bin arg {min_bin_arg}")
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    min_bin_arg_1 = min_bin_arg.astype(int)
+    ax.scatter(t_vec_combined[:,0], t_vec_combined[:,1], color = 'y')
+    ax.scatter(t_vec_combined[min_bin_arg_1[:],0], t_vec_combined[min_bin_arg_1[:],1], color= 'r')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
 
-    values_for_spline_arg_999 = np.argwhere(values_for_spline==[-999])
-    values_for_spline = np.delete(values_for_spline, values_for_spline_arg_999, axis=0)
-"""
-    """print(f"va shape …{np.shape(values_for_spline)}")
-    values_for_spline[:,0] = t_on_line
-    values_for_spline[:,1] = np.abs(vector_to_line_distances)
-    ind = np.argsort( values_for_spline[:,0] )
-    values_for_spline = values_for_spline[ind]
-    print(f" vasd {values.shape}  and values {type(values)}")
-    line_pc_array = np.ndarray.tolist(values_for_spline)
-    print(f" asdgredtz {type(line_pc_array)}  and values {len(line_pc_array)}")
+    plt.show()
 
-    b_spline = geomdl.fitting.approximate_curve(line_pc_array, 2)
-    print("nach spline")
-    b_spline.sample_size = b_spline.sample_size * 3
-    point = np.asarray(b_spline.evalpts)"""
-    vec_line_dis = vector_to_line_distances
+
+
+    line_pc_array = np.ndarray.tolist(t_vec_combined[min_bin_arg_1[:],:])
+
+    b_spline = geomdl.fitting.interpolate_curve(line_pc_array, 2)
+    local_mins = np.zeros([int(1/bin_size)])
+    for b_s in range(int(1/bin_size)):
+        t_old = b_s * bin_size
+        for k in range(10):
+            point_on_spline = b_spline.derivatives(t_old, 2)
+            f_t = (point_on_spline[0][0]) * point_on_spline[1][0] + (point_on_spline[0][1]) * point_on_spline[1][1]# + (point_on_spline[0][2]- p[2]) * point_on_spline[1][2] 
+            f__t = point_on_spline[1][0] * point_on_spline[1][0] + point_on_spline[1][1] * point_on_spline[1][1]# + point_on_spline[1][2] * point_on_spline[1][2]#+ (point_on_spline[0][0]- p[0]) * point_on_spline[2][0] + (point_on_spline[0][1]- p[1]) * point_on_spline[2][1]#+ point_on_spline[1][2] * point_on_spline[1][2] + (point_on_spline[0][0]- p[0]) * point_on_spline[2][0] + (point_on_spline[0][1]- p[1]) * point_on_spline[2][1] + (point_on_spline[0][2]- p[2]) * point_on_spline[2][2]
+            t_old = t_old - point_on_spline[1][0] / point_on_spline[2][0]#f_t / f__t  
+
+            if t_old < 0.00:
+                t_old = 0.0
+            elif t_old > 1.00:
+                t_old = 1.0
+            else: 
+                t_old = t_old  
+
+
+    
+        local_mins[b_s] = t_old
+    
+    local_mins = np.round(local_mins[:], decimals = 5)
+    local_mins = np.unique(local_mins)
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    min_bin_arg_1 = min_bin_arg.astype(int)
+    ax.scatter(t_vec_combined[:,0], t_vec_combined[:,1], color = 'y')
+    ax.scatter(local_mins, np.full(np.shape(local_mins), [0.14]), color = 'g')
+    ax.scatter(np.linspace(0, 1, int(1/bin_size)), np.full(int(1/bin_size), [0.25]), color = 'c')
+    ax.scatter(t_vec_combined[min_bin_arg_1[:],0], t_vec_combined[min_bin_arg_1[:],1], color= 'r')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+
+    plt.show()
+    """fig = plt.figure()
+    ax = fig.add_subplot()
+    min_bin_arg_1 = min_bin_arg.astype(int)
+    ax.scatter(t_vec_combined[:,0], t_vec_combined[:,1], color = 'y')
+    ax.scatter(t_vec_combined[min_bin_arg_1[:],0], t_vec_combined[min_bin_arg_1[:],1], color= 'r')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+
+    plt.show()"""
+    print(local_mins)
+
+
+            
+    
+    #b_spline.sample_size = sample_size * b_spline.sample_size
+
+
+def find_points_to_pull(t_vec_combined, vector_to_line_distances, t_on_line, bSpline):
     t_sort_indices = np.argsort(t_on_line)
     start_t = np.linspace(0, 1, 40)
     indexes = np.zeros(np.shape(start_t)[0])
     for start_p in range (np.shape(start_t)[0]):
-        start = np.argmin(np.abs(t_on_line - start_t[start_p]))
-        index_start = np.argwhere(t_sort_indices == start)
+        start = np.argmin(np.abs(t_vec_combined[:, 0] - start_t[start_p]))
+        print(t_vec_combined[:,0])
+        index_start = start
+        print(f"start _ {start}")
 
-        for i in range(20):
+        for i in range(3):
             if index_start == 0:
                 index_start = 1
 
-            m = (vec_line_dis[t_sort_indices[index_start]] - vec_line_dis[t_sort_indices[index_start-1]]) / (t_on_line[t_sort_indices[index_start]] - t_on_line[t_sort_indices[index_start-1]]+0.0001)
+            m = (t_vec_combined[index_start, 1] - t_vec_combined[index_start - 1, 1]) / (t_vec_combined[index_start, 0] - t_vec_combined[index_start - 1, 0])
             print(f"asdf {t_sort_indices[index_start]}  and {t_on_line[t_sort_indices[index_start]]} and {m}")
-            t_new = np.argmin(np.abs(t_on_line - (t_on_line[t_sort_indices[index_start]] - m)))
-            index_start = np.argwhere(t_sort_indices[t_new])
+            t_new = index_start - t_vec_combined[index_start, 1] / m
+            index_start = np.argmin(np.abs(t_vec_combined[:, 0] - t_new))
         indexes[start_p] = index_start
         
     print(f"ind {indexes}")
-    """print(np.shape(point))
     fig = plt.figure()
-    
+    indexes = indexes.astype(int, copy = False)
     ax = fig.add_subplot()
-    ax.scatter(point[:,0], point[:,1])"""
-    #ax.scatter(t_on_line, vector_to_line_distances)
-    #ax.scatter(t_on_line[t_sort_indices[indexes[:]]], vector_to_line_distances[t_sort_indices[indexes[:]]])
-    """ax.set_xlabel('x')
-    ax.set_ylabel('y')"""
+    ax.scatter(t_vec_combined[:,0], t_vec_combined[:,1], color = 'y')
+    ax.scatter(t_vec_combined[indexes[:],0], t_vec_combined[indexes[:],1], color= 'r')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
 
     plt.show()
-        
-
-    """for s_p in range(0, np.shape(start_points)[0], 1):
-            t_old = np.linspace(0, 1, 40)[s_p]
-            for k in range(20):
-                point_on_spline = bSpline.derivatives(t_old, 2)
-                f_t = (point_on_spline[0][0] - p[0]) * point_on_spline[1][0] + (point_on_spline[0][1] - p[1]) * point_on_spline[1][1] + (point_on_spline[0][2]- p[2]) * point_on_spline[1][2] 
-                f__t = point_on_spline[1][0] * point_on_spline[1][0] + point_on_spline[1][1] * point_on_spline[1][1] + point_on_spline[1][2] * point_on_spline[1][2]#+ (point_on_spline[0][0]- p[0]) * point_on_spline[2][0] + (point_on_spline[0][1]- p[1]) * point_on_spline[2][1]#+ point_on_spline[1][2] * point_on_spline[1][2] + (point_on_spline[0][0]- p[0]) * point_on_spline[2][0] + (point_on_spline[0][1]- p[1]) * point_on_spline[2][1] + (point_on_spline[0][2]- p[2]) * point_on_spline[2][2]
-                t = t_old - f_t / f__t    
-    
-                if t < 0.00:
-                    t_old = 0.0
-                elif t > 1.00:
-                    t_old = 1.0
-                else: 
-                    t_old = t"""  
-
-
-
-
-    #return pull_points
 
 
 def find_smallest_dis_to_point(pcd_colon, bspline, vector_to_line, t_on_line):
     vis = o3d.visualization.Visualizer()
     vis.create_window()
     vis.add_geometry(pcd_colon)
-    print("there")
     points = copy.deepcopy(np.asarray(pcd_colon.points))
     z = 0
     # copy by reference
     half_line = np.asarray(pcd_colon.points)
-    print("h")
     for i in range(500):
         dist_to_z = np.power(np.abs(1 - np.abs((z % 1.0) - t_on_line[:])), 4)
         dist_to_z = np.power(np.abs(1 - np.abs((z % 1.0) - t_on_line[:])), 4)
-        #print(f"dist to z {dist_to_z}")
-        
-        #disc = np.argwhere(np.abs((z % 1.0) - t_on_line[:]) > 0.5)
-        #print(f"shkpae {np.shape(disc)}")
+
         half_line[:, 0] = points[:, 0] + np.multiply(dist_to_z, vector_to_line[:, 0])
         half_line[:, 1] = points[:, 1] + np.multiply(dist_to_z, vector_to_line[:, 1])
         half_line[:, 2] = points[:, 2] + np.multiply(dist_to_z, vector_to_line[:, 2])
-        #half_line[10] = points[10] + 0.25 * vector_to_line[10]
-        #pcd_colon.points = o3d.utility.Vector3dVector(half_line)
 
         vis.poll_events()
         vis.update_geometry(pcd_colon)
@@ -292,20 +312,6 @@ def find_smallest_dis_to_point(pcd_colon, bspline, vector_to_line, t_on_line):
         sleep(0.1)
         z+=0.01
 
-
-        """half_line = points + 0.1* vector_to_line
-        
-        #half_line[10] = points[10] + 0.25 * vector_to_line[10]
-        pcd_colon.points = o3d.utility.Vector3dVector(half_line)
-
-        vis.poll_events()
-        vis.update_geometry(pcd_colon)
-        vis.update_renderer()
-        sleep(0.1)
-        z+=0.001"""
-
-
-    #vis.register_animation_callback(move_colon)
     vis.run()
     vis.destroy_window()
 
@@ -313,7 +319,7 @@ def main():
 
     # adjustable parameter
     sample_size = 2
-    normals_to_inside = False
+    normals_to_inside = True
 
 
     # prepare colon for visualization
@@ -338,7 +344,15 @@ def main():
     # read pointcloud and convert to array
     pcd = o3d.io.read_point_cloud(path_zyl_seg)
 
+    o3d.visualization.draw_geometries([pcd_colon],
+        mesh_show_wireframe = True,
+        mesh_show_back_face = True,
+        point_show_normal = True)
     
+    normals_to_inside = input("Are normals pointing to inside? yes: insert True, no: insert False:  ")
+
+
+
     line_bSpline = get_bSpline(pcd, sample_size)
 
     
@@ -378,7 +392,7 @@ def main():
         point_show_normal = True)
 
     find_smallest_dis_to_point((pcd_colon), line_bSpline, vector_to_line, t_on_line)
-    visualize(pcd_colon)
+    #visualize(pcd_colon)
 
 
 if __name__ == "__main__": main()  
